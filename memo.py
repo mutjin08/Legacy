7dd7171b0a0bb8779cc037f33603b9444fa9ed41
mutjin23 <mutjin23@gmail.com>
2023-05-12 12:54:33 +0900

    service step2

---
+++ memo.py
@@ -0,0 +1,1 @@
+{% if page_number >= question_list.number|add:-5 and page_number <= question_list.number|add:5 %}
--- pybo/views.py
+++ pybo/views.py
@@ -6,11 +6,11 @@ from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
 
 def index(request):
-    page = request.GET.get('page', '1')
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    paginator = Paginator(question_list, 10)
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': question_list}
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
